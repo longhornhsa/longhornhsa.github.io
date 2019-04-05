4a4b5073e264168dd144a7fa08351da11e17905b
Abhay Ram <abhaykram12@gmail.com>
2019-04-04 21:18:04 -0500

    Website

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Navratri", "Diwali", "Sankranti", "Hinduism Awareness Week", "Holi"],
       typeSpeed: 100,
       loop: true,
     });
